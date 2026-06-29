2aedd3c7c73f205815190158fac34892c6da6978
ohboyquentin <ohboyquentin@gmail.com>
2026-06-29 18:01:45 +0200

    bump cache v5

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Mise en cache de toutes les pages et assets
 // =============================================
 
-const CACHE_NAME = "voyage-v4";
+const CACHE_NAME = "voyage-v5";
 
 // Toutes les ressources à mettre en cache
 const FILES_TO_CACHE = [
