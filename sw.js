17fa947f2d5313713fe2068e469f523cd2f75109
ohboyquentin <ohboyquentin@gmail.com>
2026-07-01 17:05:05 +0200

    drawer

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Mise en cache complète pour usage hors-ligne
 // =============================================
 
-const CACHE_NAME = "voyage-v6";
+const CACHE_NAME = "voyage-v7";
 
 // Fichiers essentiels à pré-charger dès l'installation
 const FILES_TO_CACHE = [
